4cdfadcce5fa4edf7a5c9e49baf032acd84feefb
natrix-fork <cqrdev@gmail.com>
2019-11-08 17:41:03 +0200

    Update get_emails_from_url.py

--- emails/get_emails_from_url.py
+++ emails/get_emails_from_url.py
@@ -1,25 +1,28 @@
 import requests
 import re
 import argparse
+from validate_email import validate_email
+
 
 def get_emails(domain):
-    
     if not domain.startswith("http://") == True:
-        domain="http://"+domain
-    
+        domain = "http://" + domain
+
     response = requests.get(domain)
     text = response.text
-    patron = re.compile("[-a-zA-Z0-9._]+@[-a-zA-Z0-9_]+.[a-zA-Z0-9_.]+")
-    mails = re.findall(patron,text)
-    emails = str(mails)
+    emails = re.findall(r"\w+[a-zA-Z_0-9]+@\w+\.\w+", text)
+    emails = set(emails)
+    result = []
+    for email in emails:
+        result.append({'valid': validate_email(email), 'value': str(email)})
+    print result
 
-    print(emails)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='gets emails from domain.', prog='get_emails_from_url.py', epilog="", add_help=False)
+    parser = argparse.ArgumentParser(description='gets emails from domain.', prog='get_emails_from_url.py', epilog="",
+                                     add_help=False)
     # Adding the main options
     general = parser.add_mutually_exclusive_group(required=True)
     general.add_argument('-d', '--domain', metavar='<domain>', action='store', help='domain to be resolved.')
     args = parser.parse_args()
-    
     get_emails(args.domain)

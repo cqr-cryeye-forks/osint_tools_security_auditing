e19a8da36e6f3eede984847fffa79f403e147e5e
natrix-fork <cqrdev@gmail.com>
2019-11-08 12:59:21 +0200

    Update GeoIP.py

--- ip_map_position/GeoIP.py
+++ ip_map_position/GeoIP.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3.5
-#coding=UTF-8
+# coding=UTF-8
 
-import pygeoip 
+import pygeoip
 import os
 import sys
 import argparse
 
-class Geoip(object):
 
-	def __init__(self, target):
-		self.target = target
-		try:
-			self.gip = pygeoip.GeoIP('GeoLiteCity.dat',pygeoip.MEMORY_CACHE)
-			self.search()
-		except pygeoip.GeoIPError:
-			print("[!] You probably forgot to set the target or give an invalid target as argument.")
-		except Exception as e: 
-        		print("[!] Exception caught: {}".format(e))
+class Geoip:
+    def __init__(self, target):
+        self.target = target
+        self.gip = pygeoip.GeoIP('ip_map_position/GeoLiteCity.dat', pygeoip.MEMORY_CACHE)
+        self.search()
 
-	def search(self):
-		addr = self.target
-		rec = self.gip.record_by_addr(addr)
-		for key,val in rec.items():
-			print("[*] %s: %s" %(key, val))
-			
+    def search(self):
+        addr = self.target
+        rec = self.gip.record_by_addr(addr)
+        for key, val in rec.items():
+            print("%s: %s" % (key, val))
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Geo localizate IP addresses')
-	parser.add_argument('--target', action="store", dest="target",required=True)
-	given_args = parser.parse_args()
-	target = given_args.target
-	geoip  = Geoip(target);
-	geoip.search();		
+    parser = argparse.ArgumentParser(description='Geo localizate IP addresses')
+    parser.add_argument('--target', action="store", dest="target", required=True)
+    given_args = parser.parse_args()
+    target = given_args.target
+    geoip = Geoip(target)
+    geoip.search()

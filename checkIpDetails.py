c3b9a07681f90998abcf4b1c7b633252530ad398
Evil2997 <dima.molchanow2012@gmail.com>
2024-04-02 12:21:58 +0300

    Most of the exceptions and errors are removed

--- checkIpDetails.py
+++ checkIpDetails.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 import pathlib
+import re
 from typing import Final
 
 import dns.resolver
@@ -110,19 +111,25 @@ def checkIpDetails(query=None):
 def getGeo(domain):
     ip = ip_resolver(domain)
     response = ''
-    if not os.path.exists('GeoLite2-City.mmdb'):
-        msg = "[*] GeoLite2-City.mmdb is not found ..."
-        logging.error(msg)
+    if is_ipv4(domain):
+        pass
     else:
-        try:
+        if not os.path.exists('GeoLite2-City.mmdb'):
+            msg = "[*] GeoLite2-City.mmdb is not found ..."
+            logging.error(msg)
+        else:
             reader = geoip2.database.Reader('GeoLite2-City.mmdb')
             response = reader.city(ip)
-        except Exception as e:
-            print(e)
-            msg = "[*] AddressNotFoundError for ip: %s" % (ip)
-            logging.error(msg)
 
     return response
+
+
+def is_ipv4(address):
+    ipv4_pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    if re.match(ipv4_pattern, address):
+        return True
+    else:
+        return False
 
 
 def ip_resolver(domain):
@@ -130,8 +137,12 @@ def ip_resolver(domain):
     c_name = ''
     res = dns.resolver.Resolver()
     res.timeout = 1
+    # if is_ipv4(domain):
+    #     print("ipv4")
+    # else:
+    #     print("domain")
     try:
-        answers = dns.resolver.Resolver.resolve(domain)
+        answers = dns.resolver.resolve(domain)
         ip = str(answers[0]).split(": ")[0]
         c_name = answers.canonical_name
     except Exception as e:
@@ -157,21 +168,22 @@ if __name__ == "__main__":
     PATH_TO_OUTPUT_DIR: Final[pathlib.Path] = pathlib.Path(__file__).parent
     output_json = PATH_TO_OUTPUT_DIR / output
 
-    result = checkIpDetails(target)
+    res = checkIpDetails(target)
 
-    with open(output_json, 'w') as json_file:
-        json.dump(result, json_file, indent=2)
     try:
         response = getGeo(target)
-        # if response is not None:
-        #     if response.country is not None:
-        #         print('\nCountry:' + response.country.name)
-        #     if response.city is not None:
-        #         print('\nCity:' + response.city.name)
-        #     if response.location is not None:
-        #         print('\nLatitude' + str(response.location.latitude))
-        #         print('\nLongitude' + str(response.location.longitude))
     except Exception as e:
-        pass
+        response = None
 
-    ip_resolver(target)
+    data = {
+        "country": response.country.name if response and response.country else None,
+        "city": response.city.name if response and response.city else None,
+        "Latitude": response.location.latitude if response and response.location else None,
+        "Longitude": response.location.longitude if response and response.location else None,
+    }
+    with open(output_json, 'w') as json_file:
+        result = {
+            "result": res,
+            **data,
+        }
+        json.dump(result, json_file, indent=2)
